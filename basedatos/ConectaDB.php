adfe51f9a3ad212733661f8d0e7d9baedff6f53f
Jesus HL <csc.jesus@gmail.com>
2021-12-02 06:37:28 -0600

    Video 37 terminado

--- basedatos/ConectaDB.php
+++ basedatos/ConectaDB.php
@@ -10,13 +10,21 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "curso";
+$db = "proyecto_jsp";
 
-$conexion = mysqli_connect($host, $user, $pass, $db);
-$query = "select * from users";
+//$conexion = mysqli_connect($host, $user, $pass, $db);
+$conexion = mysqli_connect($host, $user, $pass);
+if(mysqli_connect_errno()){
+    echo"Fallo al conectar con la BD";
+}
+mysqli_select_db($conexion, $db) or die("No se encuentra la BD");
+mysqli_set_charset($conexion,"utf8");//ver acentos
+
+$query = "select * from datospersonales";
 
 $result = mysqli_query($conexion, $query);
 
 while ($fila = mysqli_fetch_array($result)) {
-    echo "$fila[0] $fila[1] $fila[2] $fila[3] $fila[4] $fila[5] $fila[6]<br>";
+   // echo "$fila[0] $fila[1] $fila[2] $fila[3] $fila[4] $fila[5] $fila[6]<br>";
+    echo "$fila[0] $fila[1] $fila[2] $fila[3] <br>";
 }
