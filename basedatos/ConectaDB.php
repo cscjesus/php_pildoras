2f7090a202dfc5b472087449dcc94deda5d92332
Jesus HL <csc.jesus@gmail.com>
2021-12-02 09:10:49 -0600

    Video 38 terminado

--- basedatos/ConectaDB.php
+++ basedatos/ConectaDB.php
@@ -1,30 +1,55 @@
-<?php
+<!DOCTYPE html>
 
-/*
- * Document   : ConectaDB
- * Created on : 1/12/2021, 07:59:04 AM
- * Author     : liz
- * @author liz
- */
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            table,td,tr{
+                width: 50%;
+                border:1px dotted #ff0000;
+                
+                margin:auto;
+            }
+        </style>
+    </head>
+    <body>
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "proyecto_jsp";
+        <?php
+        /*
+         * Document   : ConectaDB
+         * Created on : 1/12/2021, 07:59:04 AM
+         * Author     : liz
+         * @author liz
+         */
 
+//$host = "localhost";
+//$user = "root";
+//$pass = "";
+//$db = "proyecto_jsp";
+        include './datos.php';
+//require './datos.php';
 //$conexion = mysqli_connect($host, $user, $pass, $db);
-$conexion = mysqli_connect($host, $user, $pass);
-if(mysqli_connect_errno()){
-    echo"Fallo al conectar con la BD";
-}
-mysqli_select_db($conexion, $db) or die("No se encuentra la BD");
-mysqli_set_charset($conexion,"utf8");//ver acentos
+        $conexion = mysqli_connect($host, $user, $pass);
+        if (mysqli_connect_errno()) {
+            echo"Fallo al conectar con la BD";
+        }
+        mysqli_select_db($conexion, $db) or die("No se encuentra la BD");
+        mysqli_set_charset($conexion, "utf8"); //ver acentos
+//$query = "select * from datospersonales";
+        //$query = "select * from productos";
+        $query = "select * from productos where paisdeorigen='españa'";
 
-$query = "select * from datospersonales";
-
-$result = mysqli_query($conexion, $query);
-
-while ($fila = mysqli_fetch_array($result)) {
-   // echo "$fila[0] $fila[1] $fila[2] $fila[3] $fila[4] $fila[5] $fila[6]<br>";
-    echo "$fila[0] $fila[1] $fila[2] $fila[3] <br>";
-}
+        $result = mysqli_query($conexion, $query);
+        echo "<table>";
+        while ($fila = mysqli_fetch_array($result)) {
+            echo"<tr>";
+            echo "<td>$fila[0]</td><td> $fila[1]</td> <td>$fila[2]</td> <td>$fila[3]</td> <td>$fila[4]</td> <td>$fila[5]</td> <td>$fila[6]</td></tr>";
+            echo"</tr>";
+            //echo "$fila[0] $fila[1] $fila[2] $fila[3] <br>";
+        }
+        echo '</table>';
+        mysqli_close($conexion);
+        ?>
+    </body>
+</html>
---
+++ basedatos/datos.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* Document   : datos
+* Created on : 2/12/2021, 08:47:34 AM
+* Author     : liz
+* @author liz
+*/
+
+// put your code here
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "proyecto_jsp";
+?>
